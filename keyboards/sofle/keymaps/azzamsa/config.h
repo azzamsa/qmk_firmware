d2f708a521c04bfd2afaa09a4b9f4401b48d7b5f
azzamsa <me@azzamsa.com>
2021-06-21 16:19:05 +0700

    [Keymap]: enable sticky modes for tapping toggle keys

--- keyboards/sofle/keymaps/azzamsa/config.h
+++ keyboards/sofle/keymaps/azzamsa/config.h
@@ -5,8 +5,12 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define TAPPING_FORCE_HOLD  // disable ability auto-repeat
+//#define TAPPING_FORCE_HOLD  // disable ability auto-repeat. WARN! will break anything that uses tapping toggles
+
 #ifdef TAPPING_TERM
     #undef TAPPING_TERM
     #define TAPPING_TERM 200 // tapping timeout (in ms)
 #endif
+
+#define ONESHOT_TAP_TOGGLE 2  // Tapping N times holds the key until tapped once again. doesn't work!
+//#define ONESHOT_TIMEOUT 200  // Time (in ms) before the one shot key is released
