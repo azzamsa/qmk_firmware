a835c2f63d36317d575b56b526e834039d91cad1
azzamsa <me@azzamsa.com>
2021-06-21 15:46:40 +0700

    [Keymap]: need to use layer name directly for OSL to work

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-  OSM(MOD_LCTL), OSL(KC_RAISE), OSM(MOD_LALT), KC_LGUI, LCTL_T(KC_ENT),   RCTL_T(KC_SPC), KC_RGUI, OSM(MOD_RALT), OSL(KC_LOWER), OSM(MOD_RCTL) \
+  OSM(MOD_LCTL), OSL(_RAISE), OSM(MOD_LALT), KC_LGUI, LCTL_T(KC_ENT),   RCTL_T(KC_SPC), KC_RGUI, OSM(MOD_RALT), OSL(_LOWER), OSM(MOD_RCTL) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
