c8dc9fd7dc79db4c40373d26d02ba1c80f6af303
azzamsa <me@azzamsa.com>
2021-06-22 18:36:19 +0700

    [Keymap]: swap super with return/space
    My thumb rest naturally in the second outer key.
    It is easier to hit them when typing. I don't need to stretch the thumb.
    It is right under the thumb

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,  \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,                  XXXXXXX, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,   \
-  OSM(MOD_LCTL), OSL(_RAISE), OSM(MOD_LALT), OSM(MOD_LGUI), LCTL_T(KC_ENT),   RCTL_T(KC_SPC), OSM(MOD_RGUI), OSM(MOD_RALT), OSL(_LOWER), OSM(MOD_RCTL) \
+  OSM(MOD_LCTL), OSL(_RAISE), OSM(MOD_LALT), LCTL_T(KC_ENT), OSM(MOD_LGUI),    OSM(MOD_RGUI), RCTL_T(KC_SPC), OSM(MOD_RALT), OSL(_LOWER), OSM(MOD_RCTL) \
 ),
 
 /* LOWER
