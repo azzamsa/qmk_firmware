906e47fc468f582a7cacc8e0b92a988d28f25c26
azzamsa <me@azzamsa.com>
2021-06-22 11:51:00 +0700

    [Keymap]: update keyboard layout documentation

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,   \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
   _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,        _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,  \
-                       _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______\
+                    _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______\
 ),
 
 /* RAISE
@@ -81,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   _______, _______ , _______ , _______ , _______ , _______,                         _______, _______, _______,  _______ ,  _______ ,_______, \
-  _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                             KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD, KC_DLINE, KC_BSPC, \
-  _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC, \
-  _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX,  _______,        _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,  XXXXXXX,  _______, \
-                         _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______ \
+  _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX,  XXXXXXX,                          KC_PGUP, KC_PRVWD, KC_UP,   KC_NXTWD, KC_DLINE, KC_BSPC, \
+  _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,  KC_CAPS,                          KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT,  KC_DEL,   KC_BSPC, \
+  _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PASTE, XXXXXXX, _______,        _______, XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,  XXXXXXX,  _______, \
+                    _______, _______, _______,  _______, _______,        _______, _______, _______,  _______, _______ \
 )
 };
 
