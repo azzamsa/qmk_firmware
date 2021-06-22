97b6618cbd6c43ac709a3368d15235374f404bd4
azzamsa <me@azzamsa.com>
2021-06-22 18:36:45 +0700

    [Keymap]: remove sticky tap key
    I can use shift with OSL if I press them simultaneously with other key.

--- keyboards/sofle/keymaps/azzamsa/config.h
+++ keyboards/sofle/keymaps/azzamsa/config.h
@@ -5,12 +5,7 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-//#define TAPPING_FORCE_HOLD  // disable ability auto-repeat. WARN! will break anything that uses tapping toggles
-
 #ifdef TAPPING_TERM
     #undef TAPPING_TERM
-    #define TAPPING_TERM 200 // tapping timeout (in ms)
+    #define TAPPING_TERM 300 // tapping timeout (default: 200 ms).
 #endif
-
-#define ONESHOT_TAP_TOGGLE 3  // Tapping N times holds the key until tapped once again. doesn't work!
-//#define ONESHOT_TIMEOUT 200  // Time (in ms) before the one shot key is released
