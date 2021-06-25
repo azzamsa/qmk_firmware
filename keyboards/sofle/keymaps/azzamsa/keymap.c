92202628f9a35e392c7527bba388443383425106
azzamsa <me@azzamsa.com>
2021-06-25 10:18:47 +0700

    [Keymap]: add delete key

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |      |      |      |      |      |                    | PGup | Caps |      |      |      | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |      |      |      |      |      |-------.    ,-------| Left | Down | Up   | Rigth|      |      |
+ * | Tab  |      |      |      |      |      |-------.    ,-------| Left | Down | Up   | Rigth|      | Del  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |Shift |      |      |      |      |      |-------|    |-------|PGdown| App  |      |      |      | Shift|
  * `-----------------------------------------/      /      \      \-----------------------------------------'
@@ -82,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_PGUP,   KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_TRNS,  \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,     \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_PGDOWN, KC_APP,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,    \
+  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_DEL,    \
+  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_PGDOWN, KC_APP,   KC_NO,   KC_NO,   KC_NO,  KC_TRNS,    \
            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS \
 )
 };
