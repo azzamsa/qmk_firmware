c376d31e73c987789ae7c3bd2dadc00cdea7f7c4
azzamsa <me@azzamsa.com>
2021-07-12 07:43:14 +0700

    [Keymap]: use other key to activate `TO(ADJUST)` to avoid accidental press

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -74,18 +74,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |      |      |      |      |      |-------.    ,-------| Left | Down | Up   | Rigth|      | Del  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |Shift |      |      |      |      |      |-------|    |-------|      | App  |PGdown|      |      | Shift|
+ * |Shift |      |ADJUST|      |      |      |-------|    |-------|      | App  |PGdown|      |      | Shift|
  * `-----------------------------------------/      /      \      \-----------------------------------------'
- *               |   TO | OSM  | OSM  |ENTER| OSM  /        \ OSM  |SPACE| OSM  | OSM  | OSL  |
+ *               | OSL  | OSM  | OSM  |ENTER| OSM  /        \ OSM  |SPACE| OSM  | OSM  | OSL  |
  *               | RAISE| LCtr | LAlt |     | LGui/          \RGui |     | RAlt | RCtr | LOWER|
  *               `-------------------------------'            '-------------------------------'
  */
 [_RAISE] = LAYOUT( \
-  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,     KC_F7,   KC_F8,     KC_F9,   KC_F10, KC_F11,  \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_NO,     KC_CAPS, KC_PGUP,   KC_NO,   KC_NO,  KC_TRNS,  \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_LEFT,   KC_DOWN, KC_UP,     KC_RGHT, KC_NO,  KC_DEL,    \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_NO,     KC_APP,  KC_PGDOWN, KC_NO,   KC_NO,  KC_TRNS,    \
-                TO(_ADJUST), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS \
+  KC_TRNS, KC_F1, KC_F2,       KC_F3,   KC_F4,   KC_F5,                            KC_F6,     KC_F7,   KC_F8,     KC_F9,   KC_F10, KC_F11,  \
+  KC_TRNS, KC_NO, KC_NO,       KC_NO,   KC_NO,   KC_NO,                            KC_NO,     KC_CAPS, KC_PGUP,   KC_NO,   KC_NO,  KC_TRNS,  \
+  KC_TRNS, KC_NO, KC_NO,       KC_NO,   KC_NO,   KC_NO,                            KC_LEFT,   KC_DOWN, KC_UP,     KC_RGHT, KC_NO,  KC_DEL,    \
+  KC_TRNS, KC_NO, TO(_ADJUST), KC_NO,   KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_NO,     KC_APP,  KC_PGDOWN, KC_NO,   KC_NO,  KC_TRNS,    \
+                  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
