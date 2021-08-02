82b9de4f5d93bf1a55a234c0538cf4d230ce6a7a
azzamsa <me@azzamsa.com>
2021-08-03 05:37:08 +0700

    [Keymap]: change to colemak-dh layout

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+ * | ESC  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+ * | TAB  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *               | OSL  | OSM  | OSM  |ENTER| OSM  /        \ OSM  |SPACE| OSM  | OSM  | OSL  |
  *               | RAISE| LCtr | LAlt |     | LGui/          \RGui |     | RAlt | RCtr | LOWER|
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT( \
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                   KC_6,   KC_7,          KC_8,          KC_9,   KC_0,    KC_GRV, \
-  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                                   KC_J,   KC_L,          KC_U,          KC_Y,   KC_SCLN, KC_BSPC, \
-  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                                   KC_H,   KC_N,          KC_E,          KC_I,   KC_O,    KC_QUOT,  \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,           KC_NO,         KC_K,   KC_M,          KC_COMM,       KC_DOT, KC_SLSH, KC_RSFT,   \
+  KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,   KC_L,          KC_U,          KC_Y,   KC_SCLN, KC_BSPC, \
+  KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                   KC_M,   KC_N,          KC_E,          KC_I,   KC_O,    KC_QUOT,  \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,           KC_NO,         KC_K,   KC_H,          KC_COMM,       KC_DOT, KC_SLSH, KC_RSFT,   \
   OSL(_RAISE), OSM(MOD_LCTL), OSM(MOD_LALT), KC_ENT, OSM(MOD_LGUI),     OSM(MOD_RGUI), KC_SPC, OSM(MOD_RALT), OSM(MOD_RCTL), OSL(_LOWER) \
 
 ),
