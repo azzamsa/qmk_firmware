ad01cd1cb1d90a82d91bdd6e79fcad0d34904f97
azzamsa <me@azzamsa.com>
2021-08-30 16:22:04 +0700

    [Keymap]: can't change workspace in adjust mode

--- keyboards/sofle/keymaps/azzamsa/keymap.c
+++ keyboards/sofle/keymaps/azzamsa/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |      |Qwerty|Clmk  |      |      |                    | BTN1 | BTN2 | WH_U |      |      | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `-------------------------------'            '-------------------------------'
  */
 [_ADJUST] = LAYOUT( \
-  KC_TRNS, KC_F1,   KC_F2,       KC_F3,        KC_F4,   KC_F5,                            KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  KC_GRV,  KC_1,    KC_2,        KC_3,         KC_4,    KC_5,                             KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TRNS, KC_NO,   DF(_QWERTY), DF(_COLEMAK), KC_NO,   KC_NO,                            KC_BTN1,   KC_BTN2, KC_WH_U, KC_NO,   KC_NO,   KC_TRNS,  \
   KC_TRNS, KC_ACL0, KC_ACL1,     KC_ACL2,      KC_NO,   KC_NO,                            KC_MS_L,   KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   KC_DEL,    \
   KC_TRNS, KC_NO,   KC_NO,       KC_NO,        KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_NO,     KC_NO,   KC_WH_D, KC_NO,   KC_NO,   KC_TRNS,    \
